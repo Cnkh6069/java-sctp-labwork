4391386923f7b84690bdb116d5b1e4dcef6d61e1
Cnkh6069 <ngkuanhua@gmail.com>
2025-04-23 11:49:43 +0000

    lab work

---
+++ Main.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Rule of thumb: all member variables and method must be static in Main
+public class Main {
+
+    // STATIC = WHAT IS STATIC
+    // Static = all instances of the classes share that variable
+    // It exists even without creating an instance of the class
+    // Java doesn't create an instance of Main when you run the program
+    // It just directly call Main.main()
+    // static methods can only refer or use static variables and static methods
+    public static Scanner sc;
+
+    // static methods can only access static stuff
+    public static ArrayList<Product> productDatabase;
+
+    public static void main(String[] args) {
+
+        // testProducts();
+
+        // Create an empty ArrayList of product
+        productDatabase = new ArrayList<Product>();
+        while (true) {
+            sc = new Scanner(System.in);
+            System.out.println("Menu:");
+            System.out.println("1. See all products");
+            System.out.println("2. Add a new product");
+            System.out.println("3. Update a product");
+            System.out.println("4. Delete a product");
+            System.out.println("5. Quit");
+            int choice = sc.nextInt();
+            sc.nextLine(); // clear input buffer
+            if (choice == 1) {
+                showProducts();
+            }
+            if (choice == 2) {
+                addProduct();
+            }
+            if (choice == 3) {
+                System.out.println("Update a product");
+            }
+            if (choice == 4) {
+                System.out.println("Delete a product");
+            }
+            if (choice == 5) {
+                break;
+            }
+
+        }
+
+    }
+
+    public static void showProducts() {
+        System.out.println("See All Products");
+        for (Product p : productDatabase) {
+            System.out.println(p);
+        }
+    }
+
+    public static void addProduct() {
+        // First, ask the information required to construct the Parent class
+        System.out.println("Add a New Product");
+        System.out.print("Enter the name: ");
+        String name = sc.nextLine();
+        System.out.print("SKU: ");
+        String sku = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        sc.nextLine(); // clear buffer
+        System.out.println("Type of product? D=Digital, P=Physical");
+        String productType = sc.next();
+        sc.nextLine(); // clear
+
+        // check if we are adding a physical product or a digital product
+        if (productType.toLowerCase().equals("d")) {
+            // adding a new digital product
+            System.out.println("Enter the file format");
+            String fileFormat = sc.nextLine();
+            System.out.println("Enter the download link");
+            String downloadLink = sc.nextLine();
+
+            DigitalProduct product = new DigitalProduct(name, sku, price, fileFormat, downloadLink);
+            productDatabase.add(product);
+            System.out.println("Product added successfully");
+
+        } else {
+            // adding a new physical product
+            System.out.print("Please enter the size: ");
+            String size = sc.nextLine();
+            System.out.println("Please enter the weight: ");
+            double weight = sc.nextDouble();
+            sc.nextLine(); // clear the buffer
+            System.out.println("Pleae enter the color: ");
+            String color = sc.nextLine();
+
+            PhysicalProduct p = new PhysicalProduct(name, sku, price, size, weight, color);
+            productDatabase.add(p);
+            System.out.println("Product added successfully");
+        }
+
+    }
+
+    public static void testProducts() {
+        Product p = new Product();
+        p.setName("Ninentodo Switch");
+        p.setSku("NIN123");
+        p.setPrice(500);
+        System.out.println(p);
+
+        // System.out.println(p.getName());
+        // System.out.println(p.getSku());
+        // System.out.println(p.getPrice());
+
+        // catalog ArrayList store Product - and ANY class that extends from Product
+        // (direct or indirectly)
+        ArrayList<Product> catalog = new ArrayList<>();
+
+        Product p2 = new Product();
+        System.out.println(p2);
+
+        Product p3 = new Product("PSOne", "PS01", 600);
+        System.out.println(p3);
+        p3.setName("Xbox one");
+
+        PhysicalProduct book = new PhysicalProduct("Test Book", "B001", 25.5, "10cmx20cm", 200, "black");
+        System.out.println(book);
+        catalog.add(book);
+
+        DigitalProduct game = new DigitalProduct("Assassin Creed", "ARC01", 50, "zip", "https://fake.file/ac");
+        System.out.println(game);
+        catalog.add(game);
+
+        for (Product eachProduct : catalog) {
+            System.out.println(eachProduct);
+        }
+
+    }
+}
---
+++ Product.java
@@ -0,0 +1,67 @@
+// Parent class usually have the characters that shared among all the children
+public class Product {
+    protected String name;
+    protected String sku;
+    protected double price;
+
+    // A constructor is a special method
+    // Its name is the same as the class name
+    // It returns implictly an object of that class
+    // Whenever we use `new Product()` -- the constructor is called
+    // Default constructor -- it's the constructor called by default when use say new Product()
+    // it's also used to set default values for the fields
+    public Product(){
+        // the `this` in the constructor refers to the new object
+        // (aka instance of the class) that is being created
+        this.name = "Untitled Product";
+        this.sku = "0000";
+        this.price = 0;
+        // return this; // --> automatically done by the constructor
+    }
+
+    // Overloaded constructor
+    public Product(String name, String sku, double price) {
+        this.name = name;
+        this.sku = sku;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        } else {
+            // An exception is an error in the form of an object
+            // when a method throws an exception, its telling
+            // the previous method that it got a problem
+            throw new IllegalArgumentException("Invalid price");
+            
+        }
+   
+    }
+
+    // toString() is a special method
+    // it will be called when we try to an object to a string
+    public String toString() {
+        return "name = " + name + ", sku = " + sku + ", price = " + price;
+    }
+}
